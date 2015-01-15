9ebe827b03ff1267905978810fc0b1a7c72f713f
Denis Knoepfle <denis.knoepfle@sap.com>
2015-01-15 14:21:33 +0100

    Reduce sleep times for the Ramp problem
    Change-Id: I63f564e4eb0a617713187c272ebd7c1562fd1ce0
    Signed-off-by: Denis Knoepfle <denis.knoepfle@sap.com>

--- org.spotter.benchmark.app/src/org/spotter/benchmark/app/loadscripts/RampVUser.java
+++ org.spotter.benchmark.app/src/org/spotter/benchmark/app/loadscripts/RampVUser.java
@@ -2,6 +2,12 @@ package org.spotter.benchmark.app.loadscripts;
 
 import org.spotter.benchmark.app.problems.RampProblem;
 
+/**
+ * A loadscript for the Ramp.
+ * 
+ * @author Denis Knoepfle
+ * 
+ */
 public class RampVUser extends AbstractVUser {
 
 	@Override
--- org.spotter.benchmark.app/src/org/spotter/benchmark/app/problems/RampProblem.java
+++ org.spotter.benchmark.app/src/org/spotter/benchmark/app/problems/RampProblem.java
@@ -2,25 +2,34 @@ package org.spotter.benchmark.app.problems;
 
 import java.util.Random;
 
-public class RampProblem implements Problem {
+/**
+ * The Ramp problem.
+ * 
+ * @author Denis Knoepfle
+ * 
+ */
+public final class RampProblem implements Problem {
 
 	public static final String NAME = "ramp";
 
-	private static final long SLEEP_TIME_INCREASE = 100;
-	private static final long MIN_DEVIATION = 15;
-	private static final long MAX_DEVIATION = 60;
+	private static final long SLEEP_TIME_INCREASE = 6;
+	private static final long MIN_DEVIATION = 1;
+	private static final long MAX_DEVIATION = 5;
 	private static final Random RAND = new Random(System.nanoTime());
 
 	private long sleepTime;
 	private static RampProblem instance;
 
+	/**
+	 * @return the singleton instance
+	 */
 	public static synchronized RampProblem getInstance() {
 		if (instance == null) {
 			instance = new RampProblem();
 		}
 		return instance;
 	}
-	
+
 	private RampProblem() {
 		this.sleepTime = 0;
 	}
@@ -39,7 +48,6 @@ public class RampProblem implements Problem {
 	private void increaseSleepTime() {
 		long diff = MAX_DEVIATION - MIN_DEVIATION;
 		long randomDeviation = (RAND.nextLong() % (2 * diff)) - diff + MIN_DEVIATION;
-		System.out.println("calculated deviaton of " + randomDeviation);
 		this.sleepTime = this.sleepTime + SLEEP_TIME_INCREASE + randomDeviation;
 		System.out.println("new sleep time will be " + this.sleepTime + " ms");
 	}
